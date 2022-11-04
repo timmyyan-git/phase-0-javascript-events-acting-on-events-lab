4bacf7c2f4e4ff00c649d0253618938567a8b9a4
Timmy Yan <timmyyan2000@gmail.com>
2022-11-04 14:22:25 -0400

    commit tree 565dcd52c0b71e1149e1d1745f787646cb3d8022

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
